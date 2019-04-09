2ad66c93e395f8a8c6f73638af0c7daeaedf2b1b
Molly TM <mollyt@codeforamerica.org>
2019-04-09 11:41:16 -0700

    File selection input and capture path

--- navigation.js
+++ navigation.js
@@ -8,8 +8,27 @@ function showScreen3 () {
     document.getElementById("screen-3").hidden = false
 }
 
-function showSelectedCounty (countyName) {
-    document.getElementById("selected-county").innerText = countyName
+function showScreen4 () {
+    document.getElementById("screen-3").hidden = true
+    document.getElementById("screen-4").hidden = false
+}
+
+function confirmSelectedCounty (countyName) {
+    document.getElementById("county-confirmation").getElementsByClassName("selected-county")[0].innerText = countyName
+}
+
+function populateSuccessMessage (filePath, countyCode) {
+    document.getElementById("success-message").getElementsByClassName("selected-county")[0].innerText = countyCode
+    document.getElementById("success-message").getElementsByClassName("file-path")[0].innerText = filePath
+}
+
+function handleFileSelection (e) {
+    e.preventDefault()
+    e.stopPropagation()
+    let filePath = document.getElementById("doj-file").files[0].path
+    let selectedCountyCode = document.getElementById("county").value
+
+    populateSuccessMessage(filePath, selectedCountyCode)
 }
 
 document.getElementById("begin").onclick = function () {
@@ -17,6 +36,11 @@ document.getElementById("begin").onclick = function () {
 }
 
 document.getElementById("county").onchange = function (e) {
-    showSelectedCounty(e.target.selectedOptions[0].text)
+    confirmSelectedCounty(e.target.selectedOptions[0].text)
     showScreen3()
+}
+
+document.getElementById("doj-file-form").onsubmit = function (e) {
+    handleFileSelection(e)
+    showScreen4()
 }
